d87ef5114d2b730838889c4b926be9c9ce345f8b
alok <alok.kr300@gmail.com>
2025-05-01 15:59:07 +0530

    Added enum for PPTData

---
+++ src/egppt/types/ppt-enum.ts
@@ -0,0 +1,8 @@
+export enum PPTData {
+    // PPT Layout
+    PPT_LAYOUT = 'LAYOUT_16x9',
+    PPT_ASPECT_RATIO = 1.777777,
+    // PPT Theme
+    HEAD_FONT_FACE = "Arial Light",
+    BODY_FONT_FACE = "Arial"
+}
